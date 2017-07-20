5b541b5d0ee196cbb9a18ca6397e2e816c491c23
Hack Reactor Student <student@hackreactor.com>
2017-07-19 17:44:58 -0700

    Refactor app for ES6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,37 @@
-var App = (props) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><VideoPlayer video = {props.videos[1]}/></h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><VideoList videos = {props.videos}/></h5></div>
-      </div>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
 
+
+    this.state = {
+      videos: props.videos,
+      video: props.videos[0]
+    };
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><VideoPlayer video = {this.state.video}/></h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><VideoList videos = {this.state.videos}/></h5></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
